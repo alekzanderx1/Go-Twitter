f82b433974fcd887797880c1f5e4682ec14c7e63
therealbappi <chi.bharathsai@gmail.com>
2022-11-14 10:41:31 -0500

    intial commit

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 
 
 import (
-	"fmt"
 	
-	"log"
 	"net/http"
-	"os"
-	"time"
+	
 )
 
 
@@ -30,36 +27,10 @@ func public() http.Handler {
 	return http.StripPrefix("/", http.FileServer(http.Dir("./indext.html")))
 }
 
-func logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		req := fmt.Sprintf("%s %s", r.Method, r.URL)
-		log.Println(req)
-		next.ServeHTTP(w, r)
-		log.Println(req, "completed in", time.Now().Sub(start))
-	})
-}
+
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", logging(public()))
-	//mux.Handle("/", logging(index()))
-	port, ok := os.LookupEnv("PORT")
-	if !ok {
-		port = "8080"
-	}
-
-	addr := fmt.Sprintf(":%s", port)
-	server := http.Server{
-		Addr:         addr,
-		Handler:      mux,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
-		IdleTimeout:  15 * time.Second,
-	}
-	log.Println("main: running simple server on port", port)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("main: couldn't start simple server: %v\n", err)
-	}
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8080", nil)
 	
 }
