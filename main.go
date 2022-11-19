d0e75b1c4373a8e3c06db9fb9f37c585f63865b5
therealbappi <chi.bharathsai@gmail.com>
2022-11-19 09:39:36 -0500

    Updated Users Page

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
-
-
 import (
-	
-	"net/http"
 	"fmt"
-	
+	"html/template"
+	"net/http"
+	"reflect"
 )
-var users = make(map[string]string)
+
+var users = make(map[string]User)
+
+type User struct {
+	Username  string
+	password  string
+	followers [3]string
+	posts     [5]string
+}
+
+var loggeduser string
+var tp1 *template.Template
+
 //users := make(map[string]int)
 
 func signuprequest(res http.ResponseWriter, req *http.Request) {
@@ -18,14 +28,18 @@ func signuprequest(res http.ResponseWriter, req *http.Request) {
 	}
 	username := req.FormValue("username")
 	password := req.FormValue("password")
-	users[username]=password
+	temp := users[username]
+
+	temp.Username = username
+	temp.password = password
+	users[username] = temp
 	fmt.Println(users)
 	http.ServeFile(res, req, "./static/login.html")
 }
 
 func signupPage(res http.ResponseWriter, req *http.Request) {
 	http.ServeFile(res, req, "./static/signup.html")
-	
+
 }
 func loginrequest(res http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
@@ -34,27 +48,30 @@ func loginrequest(res http.ResponseWriter, req *http.Request) {
 	}
 	username := req.FormValue("username")
 	password := req.FormValue("password")
-	if _, ok := users[username]; ok{
+	if _, ok := users[username]; ok {
 		fmt.Println(users[username])
 		fmt.Println(password)
-		result1 := users[username] == password
-		if result1{
-			http.ServeFile(res, req, "./static/welcome.html")
+		temp := users[username]
+		result1 := temp.password == password
+		if result1 {
+
 			fmt.Println("Login Success")
-		}else{
+			loggeduser = username
+
+			tp1.ExecuteTemplate(res, "welcome.html", loggeduser)
+
+		} else {
 			http.ServeFile(res, req, "./static/login.html")
 			fmt.Println("Incorrect password")
 
-
 		}
 
-	}else{
+	} else {
 		fmt.Println("No user")
 
-	http.ServeFile(res, req, "./static/signup.html")
+		http.ServeFile(res, req, "./static/signup.html")
 
 	}
-
 
 }
 
@@ -62,29 +79,30 @@ func loginPage(res http.ResponseWriter, req *http.Request) {
 	http.ServeFile(res, req, "./static/login.html")
 
 }
+func userslist(res http.ResponseWriter, req *http.Request) {
 
+	//var name = "bappi"
+	fmt.Println(users)
+	keys := reflect.ValueOf(users).MapKeys()
+	type temp struct{
+		people [] string
+	}
+	peeps:=temp(
+		people:=keys,
+	)
+	tp1.ExecuteTemplate(res, "usersfeed.html", temp)
 
-
-
-
-func homePage(res http.ResponseWriter, req *http.Request) {
-	http.ServeFile(res, req, "index.html")
 }
-func public() http.Handler {
-	return http.StripPrefix("/", http.FileServer(http.Dir("./indext.html")))
-}
-
-
 
 func main() {
+	tp1, _ = tp1.ParseGlob("static/*.html")
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/signup", signupPage)
 	http.HandleFunc("/signupre", signuprequest)
 	http.HandleFunc("/loginre", loginrequest)
-	
-
+	http.HandleFunc("/users", userslist)
 
 	http.HandleFunc("/login", loginPage)
 	http.ListenAndServe(":8080", nil)
-	
+
 }
